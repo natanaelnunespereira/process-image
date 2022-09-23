08ea664c41e991ac38208b5b7293040992eebdaf
Natanael <nnunespereira123@gmail.com>
2022-09-22 21:56:15 -0300

    Alterações

--- ex2.py
+++ ex2.py
@@ -2,8 +2,8 @@ from skimage import io
 import numpy as np
 
 image = io.imread('./imagens/peppers.jpg')
-maxValue = np.amax(image)
-blue_channel = image[:,:,0]
-negativeImage = maxValue - blue_channel
-io.imsave('./imagens/peppers_negativo.jpg', negativeImage)
-io.imshow(negativeImage) 
+image[:,:,0] = np.amax(image[:,:,0]) - image[:,:,0]
+image[:,:,1] = np.amax(image[:,:,1]) - image[:,:,1]
+image[:,:,2] = np.amax(image[:,:,2]) - image[:,:,2]
+io.imsave('./imagens/peppers_negativo.jpg', image)
+io.imshow(image) 
